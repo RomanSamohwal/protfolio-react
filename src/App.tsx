e737c817f8c7bb7870ce454b8e2796a12cfdf73e
r_samohval <rsamohvwal@gmail.com>
2022-03-28 11:32:57 +0300

    add Resume component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import {Promo} from "./components/promo/Promo";
 import {About} from "./components/about/About";
+import {Resume} from "./components/resume/Resume";
 
 function App() {
     return <>
         <Promo/>
         <About/>
+        <Resume/>
     </>
 }
 
---
+++ src/components/resume/Resume.tsx
@@ -0,0 +1,73 @@
+import {Divider} from "../common/Divider";
+import university from '../../icons/experience/university.svg'
+
+export const Resume = () => {
+    return <section className='resume'>
+        <div className='container'>
+            <h2 className='title title_fz16 resume__title'>
+                Опыт
+            </h2>
+            <div className="title title_fz36 resume__subtitle">
+                Чем я буду полезна
+            </div>
+            <Divider/>
+
+            <div className="resume__wrapper">
+                <div className="resume__column">
+                    <h3 className="title title_fz20 resume__column-title">
+                        Образование
+                    </h3>
+                    <ul>
+                        <li>
+                            <div className="resume__item">
+                                <div className="resume__item-head">
+                                    <div className="resume__item-icon">
+                                        <img src={university} alt="university"/>
+                                    </div>
+                                    <h4 className='title title_fz14'>
+                                        МГТУ им. Н. Э. Баумана
+                                    </h4>
+                                    <div className="resume__item-location">
+                                        Диплом магистра | Москва (2014-2020)
+                                    </div>
+                                </div>
+                                <div className="resume__item-body">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmo.
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="resume__column">
+                    <h3 className="title title_fz20 resume__column-title">
+                        Образование
+                    </h3>
+                    <ul>
+                        <li>
+                            <div className="resume__item">
+                                <div className="resume__item-head">
+                                    <div className="resume__item-icon">
+                                        <img src={university} alt="university"/>
+                                    </div>
+                                    <h4 className='title title_fz14'>
+                                        МГТУ им. Н. Э. Баумана
+                                    </h4>
+                                    <div className="resume__item-location">
+                                        Диплом магистра | Москва (2014-2020)
+                                    </div>
+                                </div>
+                                <div className="resume__item-body">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmo.
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+}
