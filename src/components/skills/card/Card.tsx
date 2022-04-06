c55c6d7b6ee4cdbe0afbca6a510eb05a6f4d3b30
r_samohval <rsamohvwal@gmail.com>
2022-04-06 14:34:16 +0300

    Пофиксил card

--- src/components/skills/card/Card.tsx
+++ src/components/skills/card/Card.tsx
@@ -10,13 +10,13 @@ type CardProps = {
 export const Card: React.FC<CardProps> = ({title, text, img, alt}) => {
     return <div className='card'>
 
-        <div className='card__header'>
+        <div className='card-img'>
             <img src={img} alt={alt}/>
+        </div>
 
-            <div className='card__header-item title title_fz14'>
+            <div className='card__title title title_fz14'>
                 {title}
             </div>
-        </div>
 
         <div className='card__text card__text-item'>
             {text}
